49401a189d56bc4e592da5ed99f0ec98242e5e67
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-02-28 20:17:17 -0500

    Update reveal.rb

--- reveal.rb
+++ reveal.rb
@@ -2,7 +2,7 @@ class Reveal < Formula
     desc "terminal show remote in browser. From your current git project."
     homepage "https://github.com/MichaelDimmitt/gh_reveal"
     url "https://github.com/md-command-line/gh_reveal/archive/2.3.tar.gz"
-    sha256 "7b196ebb977db6ceec60039818d5c5b36b9a411482e8cf44ba49e51efb25326a"
+    sha256 "5dcfdb9813ea3cbeb247ea8c18aadfb7f6ce43be2a789ed787e809dd3935ad64"
 
     def install
         bin.install "reveal"
